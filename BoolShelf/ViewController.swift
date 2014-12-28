c43a161bf0d17cb4d1409adf5ad6954437b46188
daimaruonline <dogihara@gmail.com>
2014-12-29 00:59:01 +0900

    Initial Commit

---
+++ BoolShelf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoolShelf
+//
+//  Created by 荻原大輔 on 2014/12/29.
+//  Copyright (c) 2014年 daimaruonline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
